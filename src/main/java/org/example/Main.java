3f64d4a85692942305e09b1f3591eaeddac91176
Protrigger99 <rishi.elapati2003@gmail.com>
2024-10-16 18:18:40 +0530

    Ngrok test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -39,7 +39,7 @@ public class Main {
                     System.out.print("Enter a number: ");
                     double lnInput = scanner.nextDouble();
                     System.out.println("ln(" + lnInput + ") = " + Math.log(lnInput));
-                    logger.debug("Calculated natural logarithm ln of: " + lnInput);
+                    logger.debug("Calculated natural logarithm (ln) of: " + lnInput);
                     break;
                 case 4:
                     System.out.print("Enter the base number (x): ");
